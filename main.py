077d7cfb6801e17c206745c42da7d92867262db7
Project_Day_1 _of_100 <scosta.alexsander@gmail.com>
2022-08-24 09:47:45 -0300

    Install Commit

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ food_name = input("\nWhat's your favorite food or drink?\n\n")
 # 6. Combine the name of their city and pet and show them their band name.
 # noinspection PyUnboundLocalVariable
 band_name = f"{pet_name} {grew_up_country} {food_name}"
-print("\nYour band name could be :\n {0}".format(band_name))
+print("\nYour band name could be :\n\n {0}".format(band_name))
